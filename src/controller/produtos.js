409871c8a5efe9328f63a9ad13050768467455da
Guilherme <gui.goncalves.joi@gmail.com>
2025-11-05 19:40:38 -0300

    feat finish

--- src/controller/produtos.js
+++ src/controller/produtos.js
@@ -1,48 +1,73 @@
-import Produto from "../service/produto.js"
+import ServiceProduto from "../service/produto.js"
 
-class ControllerProduto{
-    async PegarTodos(req, res){
+class ControllerProduto {
+    async PegarTodos(req, res) {
         try {
-            const produtos=await Produto.PegarTodos()
+            const produtos = await ServiceProduto.PegarTodos()
             res.status(200).send({
                 data: produtos
             })
         } catch (error) {
-            res.status(500).send({msg: error.message})
+            res.status(500).send({ msg: error.message })
         }
     }
-    async PegarUm(req, res){
+    async PegarUm(req, res) {
         try {
-            res.status(200).send('oi')
+            // const {id} = req params
+
+            // const params = req.params
+            // params.id
+
+            const id = req.params.id
+            const produto = await ServiceProduto.PegarUm(id)
+
+            res.status(200).send({
+                data: produto
+            })
         } catch (error) {
-            res.status(500).send({msg: error.message})
+            res.status(500).send({ msg: error.message })
         }
     }
-    async Criar(req, res){
+    async Criar(req, res) {
         try {
             // const nome = req.body.nome
             // const disponivel=req.body.disponivel
             // const quantidade=req.body.quantidade
-            
-            const{nome, disponivel, quantidade}=req.body
-            await Produto.Criar(nome,disponivel,quantidade)
+
+            const { nome, disponivel, quantidade } = req.body
+            await ServiceProduto.Criar(nome, disponivel, quantidade)
             res.status(201).send()
         } catch (error) {
-            res.status(500).send({msg: error.message})
+            res.status(500).send({ msg: error.message })
         }
     }
-    async Update(req ,res){
+    async Update(req, res) {
         try {
-            res.status(200).send('oi')
+            // const{nome, disponivel, quantidade}=req.body
+            const id = req.params.id
+            const nome = req.body?.nome
+            const disponivel = req.body?.disponivel
+            const quantidade = req.body?.quantidade
+            await ServiceProduto.Update(id, nome, disponivel, quantidade)
+
+            res.status(200).send({
+                msg: 'Produto alterado'
+            })
         } catch (error) {
-            res.status(500).send({msg: error.message})
+            res.status(500).send({ msg: error.message })
         }
     }
-    async Delete(req, res){
+    async Delete(req, res) {
         try {
-            res.status(200).send('oi')
+            const id = req.params.id
+            const produto = await ServiceProduto.Delete(id)
+
+            res.status(204).send({
+                data: produto
+            })
+
         } catch (error) {
-            res.status(500).send({msg: error.message})
+            res.status(500).send({ msg: error.message })
         }
     }
 
